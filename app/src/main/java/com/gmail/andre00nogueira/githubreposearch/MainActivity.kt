7ca8ffef7dbf35f8ce6b00d52d8f3e59b095b413
André Silva <andre00nogueira@gmail.com>
2019-07-11 01:02:09 +0100

    Add Internet permission.

--- app/src/main/java/com/gmail/andre00nogueira/githubreposearch/MainActivity.kt
+++ app/src/main/java/com/gmail/andre00nogueira/githubreposearch/MainActivity.kt
@@ -2,11 +2,19 @@ package com.gmail.andre00nogueira.githubreposearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editTextQuery: EditText
+    private lateinit var buttonSearch: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editTextQuery = findViewById(R.id.editTextQuery)
+        buttonSearch = findViewById(R.id.buttonSearch)
     }
 }
