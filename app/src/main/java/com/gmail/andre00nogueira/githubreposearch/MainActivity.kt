03fccc240ecacb057924b65a555f19db692f8091
André Silva <andre00nogueira@gmail.com>
2019-07-15 00:21:02 +0100

    Fix layout of recycler view and commented the code.

--- app/src/main/java/com/gmail/andre00nogueira/githubreposearch/MainActivity.kt
+++ app/src/main/java/com/gmail/andre00nogueira/githubreposearch/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -17,85 +16,107 @@ import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
-    protected lateinit var editTextQuery: EditText
-    protected lateinit var buttonSearch: Button
-    protected lateinit var textViewTotalCount: TextView
+    private lateinit var mRepositoryName: EditText
+    private lateinit var mSearch: Button
+    private lateinit var mTotalCount: TextView
     private lateinit var mRecyclerView: RecyclerView
     private lateinit var mRepositoriesAdapter: RepositoriesAdapter
     private lateinit var repoNames: ArrayList<String>
     private lateinit var repoCreatorNames: ArrayList<String>
+    private lateinit var mSearchNewRepositories: Button
+    private lateinit var githubOwnerProfilePic: Any
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         mRecyclerView = findViewById(R.id.recyclerViewRepo) // This is the recyclerView that will contain the data
 
-        editTextQuery = findViewById(R.id.editTextQuery)
-        buttonSearch = findViewById(R.id.buttonSearch)
-        textViewTotalCount = findViewById(R.id.textViewTotalCount)
-        buttonSearch.setOnClickListener {
-            githubSearchQuery()
+        mSearchNewRepositories = findViewById(R.id.buttonSearchNewRepos)
+        mRepositoryName = findViewById(R.id.editTextQuery)
+        mSearch = findViewById(R.id.buttonSearch)
+        mTotalCount = findViewById(R.id.textViewTotalCount)
+
+
+        repoCreatorNames = ArrayList() // Init repoCreatorNames with an empty ArrayList
+        repoNames = ArrayList()// Init repoNames with an empty ArrayList
+
+
+        mSearchNewRepositories.visibility = View.GONE // When app is started, make button GONE
+
+        // When search button is pressed...
+        mSearch.setOnClickListener {
+            githubSearchQuery() // Makes the new search query
         }
 
-        repoCreatorNames = ArrayList()
-        repoNames = ArrayList()
 
-        val layoutManager: LinearLayoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
-        mRecyclerView.layoutManager = layoutManager
-        mRecyclerView.setHasFixedSize(true)
+
+        val layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false) // LinearLayoutManager because the RecyclerView will have vertical LinearLayout
+        mRecyclerView.layoutManager = layoutManager // Sets the layout manager os recycler view to the new LinearLayout we just created
+        mRecyclerView.setHasFixedSize(true) // All elements will have the same size
+
+        // When user clicks button to make a new repository search
+        mSearchNewRepositories.setOnClickListener {
+            mSearchNewRepositories.visibility = View.GONE // Makes this button GONE
+            mRecyclerView.visibility = View.GONE // Makes the current recycler view GONE
+            mRepositoryName.visibility = View.VISIBLE // Makes editText visible again
+            mSearch.visibility = View.VISIBLE // Makes button to search visible again
+        }
 
     }
 
 
     fun githubSearchQuery() {
-        val githubSearchQuery: String = editTextQuery.text.toString()
-        val url: URL = NetworkUtils.buildURL(githubSearchQuery)
-        GithubSearchAsync().execute(url)
+        val githubSearchQuery: String = mRepositoryName.text.toString() // Gets the name of the repository
+        val url: URL = NetworkUtils.buildURL(githubSearchQuery) // Builds the URL
+        GithubSearchAsync().execute(url) // And then executes it in the AsyncTask
     }
 
     inner class GithubSearchAsync : AsyncTask<URL, Void, String>() {
-        override fun onPreExecute() {
-            super.onPreExecute()
-            //textViewResponse.text = "Your response will appear here"
-        }
 
         override fun doInBackground(vararg p0: URL?): String {
-            val url: URL? = p0[0]
+            val url: URL? = p0[0] // The url passed is in the 1st position
             var githubSearchResults: String? = null
             try {
-                githubSearchResults = NetworkUtils.getResponseFromHTTPUrl(url!!)
+                githubSearchResults = NetworkUtils.getResponseFromHTTPUrl(url!!) // Gets response from HTTP in JSON
             } catch (e: IOException) {
                 e.stackTrace
             }
-            return githubSearchResults!!
+            return githubSearchResults!! // Returns null or the JSON response
         }
 
         override fun onPostExecute(result: String) {
             super.onPostExecute(result)
-            returnResponseFromJSONObject(result)
+            returnResponseFromJSONObject(result) // After the execution, calls the function that will convert JSON to actual data
         }
     }
 
 
     fun returnResponseFromJSONObject(jsonString: String){
-        val response = JSONObject(jsonString)
-        val githubTotalSearchResults = response.get("total_count")
-        textViewTotalCount.text = getString(R.string.total_counts) + githubTotalSearchResults.toString()
-        val githubArray: JSONArray = response.getJSONArray("items")
-
+        val response = JSONObject(jsonString) // Gets the JSONObject that was received in form of String
+        val githubTotalSearchResults = response.get("total_count") // Total count of repositories found
+        mTotalCount.text = getString(R.string.total_counts) + githubTotalSearchResults.toString() // Put the total of repositories found in the TextView
+        val githubArray: JSONArray = response.getJSONArray("items") //
         for (i in 0..(githubArray.length() - 1)) {
+            // For each item 0... (number of elements in github array)
             val githubRepoItem: JSONObject = githubArray.getJSONObject(i)
-            val githubRepoId = githubRepoItem.get("id")
+                // We get the repository name
             val githubRepoFullName = githubRepoItem.get("full_name")
+
+            // Owner of the repository
             val githubOwner: JSONObject = githubRepoItem.getJSONObject("owner")
-            val githubOwnerName = githubOwner.get("login")
-            repoNames.add(githubRepoFullName.toString())
-            repoCreatorNames.add(githubOwnerName.toString())
+                // Inside the owner, we get its name
+            val githubOwnerName = githubOwner.get("login") // Gets the OwnerName
+            githubOwnerProfilePic = githubOwner.get("avatar_url")
+            repoNames.add(githubRepoFullName.toString()) // Adds element to ArrayList
+            repoCreatorNames.add(githubOwnerName.toString()) // Adds element to ArrayList
         }
         
-        mRepositoriesAdapter = RepositoriesAdapter(repoNames, repoCreatorNames)
-        mRecyclerView.adapter = mRepositoriesAdapter
-        editTextQuery.visibility = View.GONE
-        buttonSearch.visibility = View.GONE
+        mRepositoriesAdapter = RepositoriesAdapter(repoNames, repoCreatorNames, githubOwnerProfilePic) // Passes the values to the RecyclerView adapter
+        mRecyclerView.adapter = mRepositoriesAdapter // Sets the adapter of the RecyclerView to the new adapter created above
+        mRecyclerView.visibility = View.VISIBLE // Sets the visibility of the RecyclerView to visible
+        mRepositoryName.visibility = View.GONE // Sets the visibility of the EditText to GONE
+        mSearch.visibility = View.GONE // Sets the visibility of the Search button to GONE
+        mSearchNewRepositories.visibility = View.VISIBLE // Sets the visibility of the new search button to visible
     }
 }
--- app/src/main/java/com/gmail/andre00nogueira/githubreposearch/RepositoriesAdapter.kt
+++ app/src/main/java/com/gmail/andre00nogueira/githubreposearch/RepositoriesAdapter.kt
@@ -5,14 +5,16 @@ import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import java.net.URL
 
-class RepositoriesAdapter(val repoNames:ArrayList<String>, val repoCreatorNames: ArrayList<String>): RecyclerView.Adapter<RepositoriesAdapter.RepositoriesAdapterViewHolder>() {
+class RepositoriesAdapter(val repoNames:ArrayList<String>, val repoCreatorNames: ArrayList<String>, val profilePic: URL): RecyclerView.Adapter<RepositoriesAdapter.RepositoriesAdapterViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepositoriesAdapterViewHolder {
-        val context: Context = parent.context
-        val repositoriesAdapterViewHolder = RepositoriesAdapterViewHolder(LayoutInflater.from(context).inflate(R.layout.repo_list_item, parent, false))
+        val context: Context = parent.context // Gets the context of MainActivity
+        val repositoriesAdapterViewHolder = RepositoriesAdapterViewHolder(LayoutInflater.from(context).inflate(R.layout.repo_list_item, parent, false)) // Creates a new view holder
         return repositoriesAdapterViewHolder
     }
 
@@ -21,14 +23,15 @@ class RepositoriesAdapter(val repoNames:ArrayList<String>, val repoCreatorNames:
     }
 
     override fun onBindViewHolder(holder: RepositoriesAdapterViewHolder, position: Int) {
-        holder.mTextViewRepoCreatorName.text = repoCreatorNames[position]
-        holder.mTextViewRepoName.text = repoNames[position]
+        holder.mTextViewRepoCreatorName.text = repoCreatorNames[position] // Sets the text view w/ the creator name
+        holder.mTextViewRepoName.text = repoNames[position] // Sets the text view w/ the repo name
 
     }
 
     inner class RepositoriesAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val mTextViewRepoCreatorName : TextView = itemView.findViewById(R.id.textViewRepoCreatorName)
         val mTextViewRepoName: TextView = itemView.findViewById(R.id.textViewRepoName)
+        val mImageViewProfilePic: ImageView = itemView.findViewById(R.id.imageViewProfilePic)
     }
 
 
